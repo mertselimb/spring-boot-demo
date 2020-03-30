6fb496c833d6002079f0599504322d3c004c2bcd
Mert Selimbeyoğlu <mertselimb@gmail.com>
2020-03-30 12:51:34 +0300

    init

---
+++ src/main/java/com/pluralsight/conferancedemo/ConferanceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferancedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferanceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferanceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/conferancedemo/ConferanceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferancedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferanceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
