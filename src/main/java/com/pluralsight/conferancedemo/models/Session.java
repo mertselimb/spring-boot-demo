540e7f14172f048a5ce14dc10c12cfbc9655460b
Mert Selimbeyoğlu <mertselimb@gmail.com>
2020-03-30 14:19:05 +0300

    add models

---
+++ src/main/java/com/pluralsight/conferancedemo/models/Session.java
@@ -0,0 +1,80 @@
+package com.pluralsight.conferancedemo.models;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "sessions")
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+    private String session_name;
+    private String session_description;
+    private Integer session_length;
+
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private Byte[] speaker_photo;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "session_speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id"))
+    private List<Speaker> speakers;
+
+    public Session() {
+    }
+
+    public Long getSession_id() {
+        return session_id;
+    }
+
+    public void setSession_id(Long session_id) {
+        this.session_id = session_id;
+    }
+
+    public String getSession_name() {
+        return session_name;
+    }
+
+    public void setSession_name(String session_name) {
+        this.session_name = session_name;
+    }
+
+    public String getSession_description() {
+        return session_description;
+    }
+
+    public void setSession_description(String session_description) {
+        this.session_description = session_description;
+    }
+
+    public Integer getSession_length() {
+        return session_length;
+    }
+
+    public void setSession_length(Integer session_length) {
+        this.session_length = session_length;
+    }
+
+    public List<Speaker> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(List<Speaker> speakers) {
+        this.speakers = speakers;
+    }
+
+    public Byte[] getSpeaker_photo() {
+        return speaker_photo;
+    }
+
+    public void setSpeaker_photo(Byte[] speaker_photo) {
+        this.speaker_photo = speaker_photo;
+    }
+}
---
+++ src/main/java/com/pluralsight/conferancedemo/repositories/SessionRepository.java
@@ -0,0 +1,4 @@
+package com.pluralsight.conferancedemo.repositories;
+
+public interface SessionRepository {
+}
